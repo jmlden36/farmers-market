0fd705a1535b5a54f94c8daf90775d8d724bf2ef
Kate Kiatsiri <keidsiri@gmail.com>
2022-02-08 14:07:39 -0800

    Update Day.js Co-authored-by: Morgan Waites <morgan.waites@gmail.com> Co-authored-by: Katie Pundt <kzpundt@gmail.com> Co-Authored-by: Matt Luker <jamesmattluker@gmail.com>

--- src/components/DayControl.js
+++ src/components/DayControl.js
@@ -1,8 +1,46 @@
 import React from "react";
-import Day from "react";
+import Day from "./Day";
+const marketSchedule = [  
+  {  
+    day: "Sunday",
+    location: "Lents International",
+    hours: "9:00am - 2:00pm",
+    booth: "4A"
+  },
+  {  
+    day: "Monday",
+    location: "Pioneer Courthouse Square",
+    hours: "10:00am - 2:00pm",
+    booth: "7C"
+  },
+  {  
+    day: "Tuesday",
+    location: "Hillsboro",
+    hours: "5:00pm - 8:30pm",
+    
+    booth: "1F"
+  },
+  {  
+    day: "Wednesday",
+    location: "Shemanski Park",
+    hours: "10:00am - 2:00pm",
+    booth: "3E"
+  },
+  {  
+    day: "Thursday",
+    location: "Northwest Portland",
+    hours: "2:00pm - 6:00pm",
+    booth: "6D"
+  },
+  {  
+    day: "Saturday",
+    location: "Beaverton",
+    hours: "10:00am - 1:30pm",
+    booth: "9G"
+  }
+];
 
 class DayControl extends React.Component {
-
   constructor(props) {
     super(props);
     this.state = {
@@ -19,11 +57,23 @@ class DayControl extends React.Component {
       dayVisibleOnPage: prevState.dayVisibleOnPage - 1
     }));
   }
+  resetDay = () => {
+    this.setState(prevState => ({
+      dayVisibleOnPage: 0
+    }));
+  }
+  
   render(){
     
     let currentlyVisibleState = null;
-    const index = this.state.dayVisibleOnPage
-    currentlyVisibleState = <Day />
+    let index = this.state.dayVisibleOnPage
+    if (index === 5){
+      index = this.resetDay
+    }
+    currentlyVisibleState = <Day day={marketSchedule[index].day}
+    location={marketSchedule[index].location}
+    hours={marketSchedule[index].hours}
+    booth={marketSchedule[index].booth}/>
     return ( 
       <React.Fragment>
         {currentlyVisibleState}
