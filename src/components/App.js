bfdfcd09072fb48cfb78563bb1e950d6b7999e7f
Kate Kiatsiri <keidsiri@gmail.com>
2022-02-08 11:38:28 -0800

    Initialize fiolders and files
    Co-Authored-by: Matt Luker <jamesmattluker@gmail.com>
    Co-authored-by: Katie Pundt <kzpundt@gmail.com>
    Co-authored-by: Morgan Waites <morgan.waites@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import DayControl from "./DayControl";
+import MonthControl from "./MonthControl";
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <DayControl />
+      <MonthControl />
+    </React.Fragment>
+
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header(){
+  return (
+    <React.Fragment>
+      <h1>Avery's Organics</h1> 
+      <h3>Come one come all and get that produce!</h3>
+    </React.Fragment>
+  );
+}
